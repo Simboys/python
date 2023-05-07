5b933822aabf7dee23029547a4966a28b107057b
Simboys <103686921+Simboys@users.noreply.github.com>
2023-05-07 03:59:24 -0500

    Update up.py

--- up.py
+++ up.py
@@ -1,4 +1,5 @@
-f1=open("t1.txt")
+f1=open("t1.txt","r")
+f2=open("r.txt","w")
 for line in f1:
     for ch in line:
         if ch.islower():
@@ -7,5 +8,6 @@ for line in f1:
         elif ch.isupper():
             ch=chr(ord(ch)+32)
             print(ch)
+        f2.write(ch)
 
 
