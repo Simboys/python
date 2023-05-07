be0a8a8778cf3154c05798e14ede157997e485c8
Simboys <103686921+Simboys@users.noreply.github.com>
2023-05-07 03:57:13 -0500

    Delete exe7.5.py

--- exe7.5.py
+++
@@ -1,9 +0,0 @@
-import re
-ip="198.32.004.4.5.3.5.3.5.3.5.3.5.3"
-x=ip.count(".")
-if x>3:
-    print("invalid")
-else:
-     string=re.sub("^[0]*"," ",ip)
-     string=re.sub("\.[0]*"," ",string)
-print(string)
